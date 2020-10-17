43c716d154c52a2fe578de94dd79dcd95a4de570
Crowdbotics <team@crowdbotics.com>
2020-10-17 17:53:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ranter_21631/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ranter_21631;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "ranter_21631";
+    }
+}
